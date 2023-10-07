946da7b68ebbe5bfbb18ed24a4fae11daddada94
l.antsifrova <larisa.antsifrova@socialtechnologies.io>
2023-10-07 18:39:40 +0300

    Added assets

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="wrapper">
+    <h2 class="title">Quoridor</h2>
+
     <GameBoard class="board" :board="board" />
+
+    <h2 class="title">Walls</h2>
+
+    <GameWalls class="walls" />
   </div>
 </template>
 
 <script>
 import { initBoard } from './services/game.js'
 import GameBoard from './components/Board.vue'
+import GameWalls from './components/Walls.vue'
 
 export default {
   name: 'App',
-  components: {GameBoard},
+  components: { GameBoard, GameWalls},
   data: function() {
   return {
     board: initBoard(),
@@ -25,12 +32,23 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .board {
+  height: 60vh;
+  margin: 0 auto 20px;
+}
+
+.walls {
+  width: 1000px;
+  height: 25vh;
   margin: 0 auto;
 }
 
 .wrapper {
-  height: 60vh;
+  height: 100vh;
+}
+
+.title {
+  text-align: center;
 }
 </style>
--- src/components/Walls.vue
+++ src/components/Walls.vue
@@ -1,13 +1,40 @@
 <template>
+  <div class="walls">
+    <img src="../assets/01.png" alt="01">
+    <img src="../assets/02.png" alt="02">
+    <img src="../assets/03.png" alt="03">
+    <img src="../assets/04.png" alt="04">
+    <img src="../assets/05.png" alt="05">
+    <img src="../assets/06.png" alt="06">
+    <img src="../assets/07.png" alt="07">
+    <img src="../assets/08.png" alt="08">
+    <img src="../assets/09.png" alt="09">
+    <img src="../assets/10.png" alt="10">
+  </div>
 
 </template>
 
 <script>
 export default {
-  name: "Walls"
+  name: "GameWalls",
 }
 </script>
 
 <style scoped>
+.walls {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 8px;
+}
 
+img {
+  width: calc(100% / 5);
+  height: 70px;
+  object-fit: contain;
+  object-position: center;
+  padding: 8px;
+  border-radius: 8px;
+  background-color: rosybrown;
+}
 </style>
